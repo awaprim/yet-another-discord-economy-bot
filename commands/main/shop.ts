41d3bd1a0d3c4f040081c92824b0725d774e2726
cldkid <emil@emil.cld>
2023-03-15 21:43:25 +0100

    add ability to view items that can be bought.

---
+++ commands/main/shop.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js';
+import { EmbedBuilder } from 'discord.js';
+const supabase = createClient(process.env["SupabaseURL"] || "", process.env["supabaseApiKey"] || "");
+const main: any = {
+  name: ["shopz"],
+  run: async (client: any, message: any, args: any) => {
+    
+    let { data: shopdata, error } = await supabase.from('shop').select('id,name,price');
+    let stufftodisplayonshop: any = ""
+    shopdata?.forEach(e=>{
+      stufftodisplayonshop += `${e.id}: **${e.name}** - ${e.price} Coins.\n`
+    })
+
+
+    const embed = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setTitle("Buy stuff for coins!")
+      .setDescription(stufftodisplayonshop)
+      .setAuthor({name:message.author.username, iconURL:message.author.displayAvatarURL()});
+      message.reply({
+          embeds: [embed]
+      });
+
+
+  }
+};
+
+export {main}

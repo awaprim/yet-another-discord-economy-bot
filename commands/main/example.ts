229e83960a7a96dfa7617af3df6ed7c8ec2f5651
cldkid <emil@emil.cld>
2023-03-15 22:07:01 +0100

    added example file to explain how to make command

---
+++ commands/main/example.ts
@@ -0,0 +1,12 @@
+const main: any = {
+  name: ["example"], // <= here you can define what's command name aka what command needs to be run to execute code
+  run: async (client: any, message: any, args: any) => {
+
+
+    // write your own code here!
+
+    
+  }
+};
+
+export {main}
